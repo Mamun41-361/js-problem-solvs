e63191da156bcf706fa7e293600a69e74d06347c
mamun <mdmamunriyan361@gmail.com>
2025-10-07 23:06:15 +0600

    gh

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 
+for(i=1; i<=100; i=i+1){
+
+    if(i %3==0 ){
+
+        document.writeln(i+ "= fizz" +"<br>" );
+       
+    }
+
+    else if(i %5==0){
+        document.writeln(i +"Buzz" );
+        
+    }
+
+    else if(i %3==0  &&  i %5==0){
+
+        document.writeln(i + "fizz" + "Buzz");
+    }
+else{
+    document.writeln(i);
+}
+//document.writeln(i + "Fizz");
+
+}
